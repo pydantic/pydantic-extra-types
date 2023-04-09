2b0a60673de65a675ab0f47a7c5faaf7172533e8
James Hutchison <122519877+JamesHutchison@users.noreply.github.com>
2023-04-09 20:12:41 +0000

    misc linting fixes

--- pydantic_extra_types/types/phone_numbers.py
+++ pydantic_extra_types/types/phone_numbers.py
@@ -1,33 +1,34 @@
+from __future__ import annotations
+
 from typing import Any, Callable, Generator
+
 from pydantic_core import PydanticCustomError, core_schema
 
-GeneratorCallableStr = Generator[Callable[[str | int], str], None, None]
+GeneratorCallableStr = Generator[Callable[..., str], None, None]
 
 
-PhoneNumberError = PydanticCustomError("value_error", "value is not a valid phone number")
+PhoneNumberError = PydanticCustomError('value_error', 'value is not a valid phone number')
 
 
 class PhoneNumber(str):
     """
     An international phone number
     """
+
     default_region_code: str | None = None
-    format: str = "RFC3966"
+    phone_format: str = 'RFC3966'
     min_length: int = 7
     max_length: int = 64
-
 
     @classmethod
     def __get_pydantic_core_schema__(cls, **_kwargs: Any) -> core_schema.AfterValidatorFunctionSchema:
         return core_schema.general_after_validator_function(
             cls.validate,
-            core_schema.str_schema(
-                min_length=cls.min_length, max_length=cls.max_length
-            ),
+            core_schema.str_schema(min_length=cls.min_length, max_length=cls.max_length),  # type: ignore
         )
 
     @classmethod
-    def validate(cls, phone_number: str, _: core_schema.ValidationInfo) -> None:
+    def validate(cls, phone_number: str, _: core_schema.ValidationInfo) -> str:
         import phonenumbers
 
         try:
@@ -37,15 +38,12 @@ class PhoneNumber(str):
         if not phonenumbers.is_valid_number(parsed_number):
             raise PhoneNumberError
 
-
-
-        return phonenumbers.format_number(
-            parsed_number, getattr(phonenumbers.PhoneNumberFormat, cls.format)
-        )
+        return phonenumbers.format_number(parsed_number, getattr(phonenumbers.PhoneNumberFormat, cls.phone_format))
 
 
 class USPhoneNumber(PhoneNumber):
     """
     A phone number that defaults to the US
     """
-    default_region_code = "US"
+
+    default_region_code = 'US'
--- tests/test_phone_numbers.py
+++ tests/test_phone_numbers.py
@@ -1,7 +1,8 @@
 from typing import Any
-from pydantic import BaseModel, ValidationError
+
 import pytest
 
+from pydantic import BaseModel, ValidationError
 from pydantic_extra_types.types.phone_numbers import PhoneNumber, USPhoneNumber
 
 
@@ -11,28 +12,19 @@ class TestPhoneNumber:
 
     # Note: the 555 area code will result in an invalid phone number
     def test_valid_phone_number(self) -> None:
-        TestPhoneNumber.Something(phone_number="+1 901 555 1212")
+        TestPhoneNumber.Something(phone_number='+1 901 555 1212')
 
     def test_when_extension_provided(self) -> None:
-        TestPhoneNumber.Something(phone_number="+1 901 555 1212 ext 12533")
+        TestPhoneNumber.Something(phone_number='+1 901 555 1212 ext 12533')
 
-
-    @pytest.mark.parametrize(
-        "invalid_number", [
-            "",
-            "123",
-            12,
-            None,
-            object()
-        ]
-    )
+    @pytest.mark.parametrize('invalid_number', ['', '123', 12, None, object()])
     def test_invalid_phone_number(self, invalid_number: Any) -> None:
         with pytest.raises(ValidationError):
-            TestPhoneNumber.Something(phone_number="55 1212")
+            TestPhoneNumber.Something(phone_number='55 1212')
 
     def test_formats_phone_number(self):
-        result = TestPhoneNumber.Something(phone_number="+1 901 555 1212 ext 12533")
-        assert result.phone_number == "tel:+1-901-555-1212;ext=12533"
+        result = TestPhoneNumber.Something(phone_number='+1 901 555 1212 ext 12533')
+        assert result.phone_number == 'tel:+1-901-555-1212;ext=12533'
 
 
 class TestUSPhoneNumber:
@@ -40,4 +32,4 @@ class TestUSPhoneNumber:
         class Something(BaseModel):
             phone_number: USPhoneNumber
 
-        Something(phone_number="(901) 555-1212")
+        Something(phone_number='(901) 555-1212')
