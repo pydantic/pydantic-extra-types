79c2ce2bab7117b950a57ae7936280320b8aa092
James Hutchison <122519877+JamesHutchison@users.noreply.github.com>
2023-04-09 05:50:44 +0000

    finish implementing phone numbers

--- pydantic_extra_types/types/phone_numbers.py
+++ pydantic_extra_types/types/phone_numbers.py
@@ -1,11 +1,10 @@
-from typing import Callable, Generator
-from pydantic import PydanticValueError, str_validator
+from typing import Any, Callable, Generator
+from pydantic_core import PydanticCustomError, core_schema
 
 GeneratorCallableStr = Generator[Callable[[str | int], str], None, None]
 
 
-class PhoneNumberError(PydanticValueError):
-    msg_template = "value is not a valid phone number"
+PhoneNumberError = PydanticCustomError("value_error", "value is not a valid phone number")
 
 
 class PhoneNumber(str):
@@ -13,26 +12,34 @@ class PhoneNumber(str):
     An international phone number
     """
     default_region_code: str | None = None
-    MAX_LENGTH: int = 64
+    min_length: int = 7
+    max_length: int = 64
+
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, **_kwargs: Any) -> core_schema.AfterValidatorFunctionSchema:
+        return core_schema.general_after_validator_function(
+            cls.validate,
+            core_schema.str_schema(
+                min_length=cls.min_length, max_length=cls.max_length
+            ),
+        )
 
     @classmethod
     def __get_validators__(cls) -> GeneratorCallableStr:
-        yield str_validator
+        # yield str_validator
         yield cls.validate
 
     @classmethod
-    def validate(cls, phone_number: str) -> None:
-        if len(phone_number) > cls.MAX_LENGTH:
-            raise PhoneNumberError()
-
+    def validate(cls, phone_number: str, _: core_schema.ValidationInfo) -> None:
         import phonenumbers
 
         try:
             parsed_number = phonenumbers.parse(phone_number, cls.default_region_code)
         except phonenumbers.phonenumberutil.NumberParseException as exc:
-            raise PhoneNumberError() from exc
+            raise PhoneNumberError from exc
         if not phonenumbers.is_valid_number(parsed_number):
-            raise PhoneNumberError()
+            raise PhoneNumberError
 
 
 class USPhoneNumber(PhoneNumber):
--- tests/test_phone_numbers.py
+++ tests/test_phone_numbers.py
@@ -1,1 +1,47 @@
-# TODO
+
+
+from typing import Any
+from pydantic import BaseModel, ValidationError
+import pytest
+
+from pydantic_extra_types.types.phone_numbers import PhoneNumber, USPhoneNumber
+
+
+class TestPhoneNumber:
+    # Note: the 555 area code will result in an invalid phone number
+    def test_valid_phone_number(self) -> None:
+        class Something(BaseModel):
+            phone_number: PhoneNumber
+
+        Something(phone_number="+1 901 555 1212")
+
+    def test_when_extension_provided(self) -> None:
+        class Something(BaseModel):
+            phone_number: PhoneNumber
+
+        Something(phone_number="+1 901 555 1212 ext 12533")
+
+
+    @pytest.mark.parametrize(
+        "invalid_number", [
+            "",
+            "123",
+            12,
+            None,
+            object()
+        ]
+    )
+    def test_invalid_phone_number(self, invalid_number: Any) -> None:
+        class Something(BaseModel):
+            phone_number: PhoneNumber
+
+        with pytest.raises(ValidationError):
+            Something(phone_number="55 1212")
+
+
+class TestUSPhoneNumber:
+    def test_defaults_to_us(self) -> None:
+        class Something(BaseModel):
+            phone_number: USPhoneNumber
+
+        Something(phone_number="(901) 555-1212")
