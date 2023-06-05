cbe85b0c102a8b76e35267223e99efc3cce82717
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-06-05 16:18:44 +0200

    👷 Add coverage check on the pipeline

--- pydantic_extra_types/payment.py
+++ pydantic_extra_types/payment.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
+
 from enum import Enum
-from typing import ClassVar, Type, TypeVar
+from typing import Any, ClassVar, TypeVar
 
 from pydantic import GetCoreSchemaHandler
 from pydantic_core import PydanticCustomError, core_schema
@@ -40,7 +42,7 @@ class PaymentCardNumber(str):
         self.brand = self.validate_brand(card_number)
 
     @classmethod
-    def __get_pydantic_core_schema__(cls, source: Type[T], handler: GetCoreSchemaHandler) -> core_schema.CoreSchema:
+    def __get_pydantic_core_schema__(cls, source: type[Any], handler: GetCoreSchemaHandler) -> core_schema.CoreSchema:
         return core_schema.general_after_validator_function(
             cls.validate,
             core_schema.str_schema(
@@ -49,7 +51,7 @@ class PaymentCardNumber(str):
         )
 
     @classmethod
-    def validate(cls, __input_value: str, _: core_schema.ValidationInfo) -> 'PaymentCardNumber':
+    def validate(cls, __input_value: str, _: core_schema.ValidationInfo) -> PaymentCardNumber:
         return cls(__input_value)
 
     @property
