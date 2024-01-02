630ef92f3afd5ba3c97ee3fc758a534c70a09ec0
Yasser Tahiri <yasserth19@gmail.com>
2024-01-02 17:49:52 +0100

    fix linting issue

--- tests/test_json_schema.py
+++ tests/test_json_schema.py
@@ -10,10 +10,10 @@ from pydantic_extra_types.country import (
     CountryOfficialName,
     CountryShortName,
 )
+from pydantic_extra_types.isbn import ISBN
 from pydantic_extra_types.mac_address import MacAddress
 from pydantic_extra_types.payment import PaymentCardNumber
 from pydantic_extra_types.ulid import ULID
-from pydantic_extra_types.isbn import ISBN
 
 
 @pytest.mark.parametrize(
