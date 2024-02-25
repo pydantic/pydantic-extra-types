b15b3d014759e53b1239295360d30cd263faed6e
EkberHasanov <ekber.022004@gmail.com>
2024-02-25 23:33:24 +0400

    upgrading version of pandas

--- pydantic_extra_types/pandas_types.py
+++ pydantic_extra_types/pandas_types.py
@@ -1,16 +1,16 @@
-from typing import Any, Type, TypeVar
+from __future__ import annotations
+
+from typing import Any
 
 import pandas as pd
 from pydantic import GetCoreSchemaHandler
 from pydantic_core import core_schema
 
-T = TypeVar('T', str, bytes, bool, int, float, complex, pd.Timestamp, pd.Timedelta, pd.Period)
-
 
 class Series(pd.Series):  # type: ignore
     @classmethod
     def __get_pydantic_core_schema__(
-        cls, source: Type[Any], handler: GetCoreSchemaHandler
+        cls, source: type[Any], handler: GetCoreSchemaHandler
     ) -> core_schema.AfterValidatorFunctionSchema:
         return core_schema.general_after_validator_function(
             cls._validate,
@@ -18,5 +18,5 @@ class Series(pd.Series):  # type: ignore
         )
 
     @classmethod
-    def _validate(cls, __input_value: Any, _: core_schema.ValidationInfo) -> 'Series':
+    def _validate(cls, __input_value: Any, _: core_schema.ValidationInfo) -> Series:
         return cls(__input_value)
