1fff4e338ef191b13e6ccfb922a9751a7fe3f879
Yasser Tahiri <yasserth19@gmail.com>
2024-02-26 16:44:46 +0100

    :memo: Add ISO 639-3 and ISO 639-5 Documentation

--- pydantic_extra_types/language_code.py
+++ pydantic_extra_types/language_code.py
@@ -1,3 +1,6 @@
+"""
+Language definitions that are based on the [ISO 639-3](https://en.wikipedia.org/wiki/ISO_639-3) & [ISO 639-5](https://en.wikipedia.org/wiki/ISO_639-5).
+"""
 from __future__ import annotations
 
 from typing import Any
@@ -15,12 +18,41 @@ except ModuleNotFoundError:  # pragma: no cover
 
 
 class ISO639_3(str):
-    # noinspection PyUnresolvedReferences
+    """ISO639_3 parses Language in the [ISO 639-3 alpha-3](https://en.wikipedia.org/wiki/ISO_639-3_alpha-3)
+    format.
+
+    ```py
+    from pydantic import BaseModel
+
+    from pydantic_extra_types.language_code import ISO639_3
+
+    class Language(BaseModel):
+        alpha_3: ISO639_3
+
+    lang = Language(alpha_3='ssr')
+    print(lang)
+    # > alpha_3='ssr'
+    ```
+    """
+
     allowed_values_list = [lang.alpha_3 for lang in pycountry.languages]
     allowed_values = set(allowed_values_list)
 
     @classmethod
     def _validate(cls, __input_value: str, _: core_schema.ValidationInfo) -> ISO639_3:
+        """
+        Validate a ISO 639-3 language code from the provided str value.
+
+        Args:
+            __input_value: The str value to be validated.
+            _: The Pydantic ValidationInfo.
+
+        Returns:
+            The validated ISO 639-3 language code.
+
+        Raises:
+            PydanticCustomError: If the ISO 639-3 language code is not valid.
+        """
         if __input_value not in cls.allowed_values:
             raise PydanticCustomError(
                 'ISO649_3', 'Invalid ISO 639-3 language code. See https://en.wikipedia.org/wiki/ISO_639-3'
@@ -31,6 +63,17 @@ class ISO639_3(str):
     def __get_pydantic_core_schema__(
         cls, _: type[Any], __: GetCoreSchemaHandler
     ) -> core_schema.AfterValidatorFunctionSchema:
+        """
+        Return a Pydantic CoreSchema with the ISO 639-3 language code validation.
+
+        Args:
+            _: The source type.
+            __: The handler to get the CoreSchema.
+
+        Returns:
+            A Pydantic CoreSchema with the ISO 639-3 language code validation.
+
+        """
         return core_schema.with_info_after_validator_function(
             cls._validate,
             core_schema.str_schema(min_length=3, max_length=3),
@@ -40,19 +83,59 @@ class ISO639_3(str):
     def __get_pydantic_json_schema__(
         cls, schema: core_schema.CoreSchema, handler: GetJsonSchemaHandler
     ) -> dict[str, Any]:
+        """
+        Return a Pydantic JSON Schema with the ISO 639-3 language code validation.
+
+        Args:
+            schema: The Pydantic CoreSchema.
+            handler: The handler to get the JSON Schema.
+
+        Returns:
+            A Pydantic JSON Schema with the ISO 639-3 language code validation.
+
+        """
         json_schema = handler(schema)
         json_schema.update({'enum': cls.allowed_values_list})
         return json_schema
 
 
 class ISO639_5(str):
-    # noinspection PyUnresolvedReferences
+    """ISO639_5 parses Language in the [ISO 639-5 alpha-3](https://en.wikipedia.org/wiki/ISO_639-5_alpha-3)
+    format.
+
+    ```py
+    from pydantic import BaseModel
+
+    from pydantic_extra_types.language_code import ISO639_5
+
+    class Language(BaseModel):
+        alpha_3: ISO639_5
+
+    lang = Language(alpha_3='gem')
+    print(lang)
+    # > alpha_3='gem'
+    ```
+    """
+
     allowed_values_list = [lang.alpha_3 for lang in pycountry.language_families]
     allowed_values_list.sort()
     allowed_values = set(allowed_values_list)
 
     @classmethod
     def _validate(cls, __input_value: str, _: core_schema.ValidationInfo) -> ISO639_5:
+        """
+        Validate a ISO 639-5 language code from the provided str value.
+
+        Args:
+            __input_value: The str value to be validated.
+            _: The Pydantic ValidationInfo.
+
+        Returns:
+            The validated ISO 639-3 language code.
+
+        Raises:
+            PydanticCustomError: If the ISO 639-5 language code is not valid.
+        """
         if __input_value not in cls.allowed_values:
             raise PydanticCustomError(
                 'ISO649_5', 'Invalid ISO 639-5 language code. See https://en.wikipedia.org/wiki/ISO_639-5'
@@ -63,6 +146,17 @@ class ISO639_5(str):
     def __get_pydantic_core_schema__(
         cls, _: type[Any], __: GetCoreSchemaHandler
     ) -> core_schema.AfterValidatorFunctionSchema:
+        """
+        Return a Pydantic CoreSchema with the ISO 639-5 language code validation.
+
+        Args:
+            _: The source type.
+            __: The handler to get the CoreSchema.
+
+        Returns:
+            A Pydantic CoreSchema with the ISO 639-5 language code validation.
+
+        """
         return core_schema.with_info_after_validator_function(
             cls._validate,
             core_schema.str_schema(min_length=3, max_length=3),
@@ -72,6 +166,17 @@ class ISO639_5(str):
     def __get_pydantic_json_schema__(
         cls, schema: core_schema.CoreSchema, handler: GetJsonSchemaHandler
     ) -> dict[str, Any]:
+        """
+        Return a Pydantic JSON Schema with the ISO 639-5 language code validation.
+
+        Args:
+            schema: The Pydantic CoreSchema.
+            handler: The handler to get the JSON Schema.
+
+        Returns:
+            A Pydantic JSON Schema with the ISO 639-5 language code validation.
+
+        """
         json_schema = handler(schema)
         json_schema.update({'enum': cls.allowed_values_list})
         return json_schema
