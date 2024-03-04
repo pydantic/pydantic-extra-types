f254b34fc5078e447efb113648efbfc8861d2120
Olivier Delmarcelle <66041410+odelmarcelle@users.noreply.github.com>
2024-03-04 23:11:57 +0100

    Added `LanguageAlpha2` and `LanguageName` types (#153)
    * Added LanguageAlpha2 type
    
    * :memo: Add language code validation and documentation
    
    ---------
    
    Co-authored-by: Yasser Tahiri <yasserth19@gmail.com>

--- pydantic_extra_types/language_code.py
+++ pydantic_extra_types/language_code.py
@@ -4,7 +4,9 @@ Language definitions that are based on the [ISO 639-3](https://en.wikipedia.org/
 
 from __future__ import annotations
 
-from typing import Any
+from dataclasses import dataclass
+from functools import lru_cache
+from typing import Any, Union
 
 from pydantic import GetCoreSchemaHandler, GetJsonSchemaHandler
 from pydantic_core import PydanticCustomError, core_schema
@@ -16,6 +18,213 @@ except ModuleNotFoundError:  # pragma: no cover
         'The `language_code` module requires "pycountry" to be installed.'
         ' You can install it with "pip install pycountry".'
     )
+
+
+@dataclass
+class LanguageInfo:
+    """
+    LanguageInfo is a dataclass that contains the language information.
+
+    Args:
+        alpha2: The language code in the [ISO 639-1 alpha-2](https://en.wikipedia.org/wiki/ISO_639-1) format.
+        alpha3: The language code in the [ISO 639-3 alpha-3](https://en.wikipedia.org/wiki/ISO_639-3) format.
+        name: The language name.
+    """
+
+    alpha2: Union[str, None]
+    alpha3: str
+    name: str
+
+
+@lru_cache
+def _languages() -> list[LanguageInfo]:
+    """
+    Return a list of LanguageInfo objects containing the language information.
+
+    Returns:
+        A list of LanguageInfo objects containing the language information.
+    """
+    return [
+        LanguageInfo(
+            alpha2=getattr(language, 'alpha_2', None),
+            alpha3=language.alpha_3,
+            name=language.name,
+        )
+        for language in pycountry.languages
+    ]
+
+
+@lru_cache
+def _index_by_alpha2() -> dict[str, LanguageInfo]:
+    """
+    Return a dictionary with the language code in the [ISO 639-1 alpha-2](https://en.wikipedia.org/wiki/ISO_639-1) format as the key and the LanguageInfo object as the value.
+    """
+    return {language.alpha2: language for language in _languages() if language.alpha2 is not None}
+
+
+@lru_cache
+def _index_by_alpha3() -> dict[str, LanguageInfo]:
+    """
+    Return a dictionary with the language code in the [ISO 639-3 alpha-3](https://en.wikipedia.org/wiki/ISO_639-3) format as the key and the LanguageInfo object as the value.
+    """
+    return {language.alpha3: language for language in _languages()}
+
+
+@lru_cache
+def _index_by_name() -> dict[str, LanguageInfo]:
+    """
+    Return a dictionary with the language name as the key and the LanguageInfo object as the value.
+    """
+    return {language.name: language for language in _languages()}
+
+
+class LanguageAlpha2(str):
+    """LanguageAlpha2 parses languages codes in the [ISO 639-1 alpha-2](https://en.wikipedia.org/wiki/ISO_639-1)
+    format.
+
+    ```py
+    from pydantic import BaseModel
+
+    from pydantic_extra_types.language_code import LanguageAlpha2
+
+    class Movie(BaseModel):
+        audio_lang: LanguageAlpha2
+        subtitles_lang: LanguageAlpha2
+
+    movie = Movie(audio_lang='de', subtitles_lang='fr')
+    print(movie)
+    #> audio_lang='de' subtitles_lang='fr'
+    ```
+    """
+
+    @classmethod
+    def _validate(cls, __input_value: str, _: core_schema.ValidationInfo) -> LanguageAlpha2:
+        """
+        Validate a language code in the ISO 639-1 alpha-2 format from the provided str value.
+
+        Args:
+            __input_value: The str value to be validated.
+            _: The Pydantic ValidationInfo.
+
+        Returns:
+            The validated language code in the ISO 639-1 alpha-2 format.
+        """
+        if __input_value not in _index_by_alpha2():
+            raise PydanticCustomError('language_alpha2', 'Invalid language alpha2 code')
+        return cls(__input_value)
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source: type[Any], handler: GetCoreSchemaHandler
+    ) -> core_schema.AfterValidatorFunctionSchema:
+        """
+        Return a Pydantic CoreSchema with the language code in the ISO 639-1 alpha-2 format validation.
+
+        Args:
+            source: The source type.
+            handler: The handler to get the CoreSchema.
+
+        Returns:
+            A Pydantic CoreSchema with the language code in the ISO 639-1 alpha-2 format validation.
+        """
+        return core_schema.with_info_after_validator_function(
+            cls._validate,
+            core_schema.str_schema(to_lower=True),
+        )
+
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls, schema: core_schema.CoreSchema, handler: GetJsonSchemaHandler
+    ) -> dict[str, Any]:
+        """
+        Return a Pydantic JSON Schema with the language code in the ISO 639-1 alpha-2 format validation.
+
+        Args:
+            schema: The Pydantic CoreSchema.
+            handler: The handler to get the JSON Schema.
+
+        Returns:
+            A Pydantic JSON Schema with the language code in the ISO 639-1 alpha-2 format validation.
+        """
+        json_schema = handler(schema)
+        json_schema.update({'pattern': r'^\w{2}$'})
+        return json_schema
+
+    @property
+    def alpha3(self) -> str:
+        """The language code in the [ISO 639-3 alpha-3](https://en.wikipedia.org/wiki/ISO_639-3) format."""
+        return _index_by_alpha2()[self].alpha3
+
+    @property
+    def name(self) -> str:
+        """The language name."""
+        return _index_by_alpha2()[self].name
+
+
+class LanguageName(str):
+    """LanguageName parses languages names listed in the [ISO 639-3 standard](https://en.wikipedia.org/wiki/ISO_639-3)
+    format.
+
+    ```py
+    from pydantic import BaseModel
+
+    from pydantic_extra_types.language_code import LanguageName
+
+    class Movie(BaseModel):
+        audio_lang: LanguageName
+        subtitles_lang: LanguageName
+
+    movie = Movie(audio_lang='Dutch', subtitles_lang='Mandarin Chinese')
+    print(movie)
+    #> audio_lang='Dutch' subtitles_lang='Mandarin Chinese'
+    ```
+    """
+
+    @classmethod
+    def _validate(cls, __input_value: str, _: core_schema.ValidationInfo) -> LanguageName:
+        """
+        Validate a language name from the provided str value.
+
+        Args:
+            __input_value: The str value to be validated.
+            _: The Pydantic ValidationInfo.
+
+        Returns:
+            The validated language name.
+        """
+        if __input_value not in _index_by_name():
+            raise PydanticCustomError('language_name', 'Invalid language name')
+        return cls(__input_value)
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source: type[Any], handler: GetCoreSchemaHandler
+    ) -> core_schema.AfterValidatorFunctionSchema:
+        """
+        Return a Pydantic CoreSchema with the language name validation.
+
+        Args:
+            source: The source type.
+            handler: The handler to get the CoreSchema.
+
+        Returns:
+            A Pydantic CoreSchema with the language name validation.
+        """
+        return core_schema.with_info_after_validator_function(
+            cls._validate,
+            core_schema.str_schema(),
+            serialization=core_schema.to_string_ser_schema(),
+        )
+
+    @property
+    def alpha2(self) -> Union[str, None]:
+        """The language code in the [ISO 639-1 alpha-2](https://en.wikipedia.org/wiki/ISO_639-1) format. Does not exist for all languages."""
+        return _index_by_name()[self].alpha2
+
+    @property
+    def alpha3(self) -> str:
+        """The language code in the [ISO 639-3 alpha-3](https://en.wikipedia.org/wiki/ISO_639-3) format."""
+        return _index_by_name()[self].alpha3
 
 
 class ISO639_3(str):
