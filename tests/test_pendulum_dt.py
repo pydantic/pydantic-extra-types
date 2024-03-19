c0657e5965f96b5cc1d009ac4aa716cec2287333
Ryan Wood <ryanwood1193@gmail.com>
2024-03-20 00:24:48 +0900

    Added support for pendulum Dates (#154)
    * Added support for pydantic Dates
    
    * fixed linting
    
    * Fixed import sorting
    
    * Fixed tests
    
    * Fixed pre-commit
    
    * Modified tests to use Date explicitly
    
    * Split out tests into date/dt components
    
    * Fixed failing test; coverage 100%

--- tests/test_pendulum_dt.py
+++ tests/test_pendulum_dt.py
@@ -2,11 +2,15 @@ import pendulum
 import pytest
 from pydantic import BaseModel, ValidationError
 
-from pydantic_extra_types.pendulum_dt import DateTime
+from pydantic_extra_types.pendulum_dt import Date, DateTime
 
 
-class Model(BaseModel):
+class DtModel(BaseModel):
     dt: DateTime
+
+
+class DateModel(BaseModel):
+    d: Date
 
 
 def test_pendulum_dt_existing_instance():
@@ -14,8 +18,17 @@ def test_pendulum_dt_existing_instance():
     Verifies that constructing a model with an existing pendulum dt doesn't throw.
     """
     now = pendulum.now()
-    model = Model(dt=now)
+    model = DtModel(dt=now)
     assert model.dt == now
+
+
+def test_pendulum_date_existing_instance():
+    """
+    Verifies that constructing a model with an existing pendulum date doesn't throw.
+    """
+    today = pendulum.today().date()
+    model = DateModel(d=today)
+    assert model.d == today
 
 
 @pytest.mark.parametrize(
@@ -26,8 +39,17 @@ def test_pendulum_dt_from_serialized(dt):
     Verifies that building an instance from serialized, well-formed strings decode properly.
     """
     dt_actual = pendulum.parse(dt)
-    model = Model(dt=dt)
+    model = DtModel(dt=dt)
     assert model.dt == dt_actual
+
+
+def test_pendulum_date_from_serialized():
+    """
+    Verifies that building an instance from serialized, well-formed strings decode properly.
+    """
+    date_actual = pendulum.parse('2024-03-18').date()
+    model = DateModel(d='2024-03-18')
+    assert model.d == date_actual
 
 
 @pytest.mark.parametrize('dt', [None, 'malformed', pendulum.now().to_iso8601_string()[:5], 42])
@@ -36,4 +58,13 @@ def test_pendulum_dt_malformed(dt):
     Verifies that the instance fails to validate if malformed dt are passed.
     """
     with pytest.raises(ValidationError):
-        Model(dt=dt)
+        DtModel(dt=dt)
+
+
+@pytest.mark.parametrize('date', [None, 'malformed', pendulum.today().to_iso8601_string()[:5], 42])
+def test_pendulum_date_malformed(date):
+    """
+    Verifies that the instance fails to validate if malformed date are passed.
+    """
+    with pytest.raises(ValidationError):
+        DateModel(d=date)
