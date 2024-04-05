666c6783810436cbb64c323c51fa9b481299fd2d
Tempookian <124298725+tempookian@users.noreply.github.com>
2024-04-05 21:21:28 +0200

    Add support for pendulum Duration

--- tests/test_pendulum_dt.py
+++ tests/test_pendulum_dt.py
@@ -2,7 +2,7 @@ import pendulum
 import pytest
 from pydantic import BaseModel, ValidationError
 
-from pydantic_extra_types.pendulum_dt import Date, DateTime
+from pydantic_extra_types.pendulum_dt import Date, DateTime, Duration
 
 
 class DtModel(BaseModel):
@@ -11,6 +11,10 @@ class DtModel(BaseModel):
 
 class DateModel(BaseModel):
     d: Date
+
+
+class DurationModel(BaseModel):
+    delta_t: Duration
 
 
 def test_pendulum_dt_existing_instance():
@@ -31,8 +35,23 @@ def test_pendulum_date_existing_instance():
     assert model.d == today
 
 
+def test_pendulum_duration_existing_instance():
+    """
+    Verifies that constructing a model with an existing pendulum duration doesn't throw.
+    """
+    delta_t = pendulum.duration(days=42, hours=13, minutes=37)
+    model = DurationModel(delta_t=delta_t)
+
+    assert model.delta_t.total_seconds() == delta_t.total_seconds()
+
+
 @pytest.mark.parametrize(
-    'dt', [pendulum.now().to_iso8601_string(), pendulum.now().to_w3c_string(), pendulum.now().to_iso8601_string()]
+    'dt',
+    [
+        pendulum.now().to_iso8601_string(),
+        pendulum.now().to_w3c_string(),
+        pendulum.now().to_iso8601_string(),
+    ],
 )
 def test_pendulum_dt_from_serialized(dt):
     """
@@ -52,6 +71,25 @@ def test_pendulum_date_from_serialized():
     assert model.d == date_actual
 
 
+@pytest.mark.parametrize(
+    'delta_t_str',
+    [
+        'P3.14D',
+        'PT404H',
+        'P1DT25H',
+        'P2W',
+        'P10Y10M10D',
+    ],
+)
+def test_pendulum_duration_from_serialized(delta_t_str):
+    """
+    Verifies that building an instance from serialized, well-formed strings decode properly.
+    """
+    true_delta_t = pendulum.parse(delta_t_str)
+    model = DurationModel(delta_t=delta_t_str)
+    assert model.delta_t == true_delta_t
+
+
 @pytest.mark.parametrize('dt', [None, 'malformed', pendulum.now().to_iso8601_string()[:5], 42])
 def test_pendulum_dt_malformed(dt):
     """
@@ -68,3 +106,15 @@ def test_pendulum_date_malformed(date):
     """
     with pytest.raises(ValidationError):
         DateModel(d=date)
+
+
+@pytest.mark.parametrize(
+    'delta_t',
+    [None, 'malformed', pendulum.today().to_iso8601_string()[:5], 42, '12m'],
+)
+def test_pendulum_duration_malformed(delta_t):
+    """
+    Verifies that the instance fails to validate if malformed durations are passed.
+    """
+    with pytest.raises(ValidationError):
+        DurationModel(delta_t=delta_t)
