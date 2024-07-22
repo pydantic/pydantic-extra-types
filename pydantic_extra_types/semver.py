343a88f0998ee9121587a5d572c4dcc5edbca6a5
James Baker Kroner <9373098+jbkroner@users.noreply.github.com>
2024-07-21 22:05:03 -0500

    initial implementation of _VersionPydanticAnnotation + tests

--- pydantic_extra_types/semver.py
+++ pydantic_extra_types/semver.py
@@ -4,25 +4,52 @@ The SemVer module provides functionality to parse and validate Semantic Versioni
 This class depends on the [semver](https://python-semver.readthedocs.io/en/latest/index.html) package.
 """
 
-try:
-    from semver import Version
-except ModuleNotFoundError as e:  # pragma: no cover
-    raise RuntimeError(
-        '`SemVer` requires "semver" to be installed. You can install it with "pip install semver"'
-    ) from e
+from typing import Annotated, Any, Callable
+from pydantic import GetJsonSchemaHandler
+from pydantic_core import core_schema
+from pydantic.json_schema import JsonSchemaValue
+from semver import Version
 
 
-class SemVer(Version):
+class _VersionPydanticAnnotation(Version):
     """
     Represents a Semantic Versioning (SemVer).
 
-    This is a wrapper around the [`semver.Version`](https://python-semver.readthedocs.io/en/latest/index.html) class.
+    Warps the `version` type from `semver`.
     """
 
     @classmethod
-    def __get_validators__(cls):
-        yield cls.parse
+    def __get_pydantic_core_schema__(
+        cls,
+        _source_type: Any,
+        _handler: Callable[[Any], core_schema.CoreSchema],
+    ) -> core_schema.CoreSchema:
+        def validate_from_str(value: str) -> Version:
+            return Version.parse(value)
+
+        from_str_schema = core_schema.chain_schema(
+            [
+                core_schema.str_schema(),
+                core_schema.no_info_plain_validator_function(validate_from_str),
+            ]
+        )
+
+        return core_schema.json_or_python_schema(
+            json_schema=from_str_schema,
+            python_schema=core_schema.union_schema(
+                [
+                    core_schema.is_instance_schema(Version),
+                    from_str_schema,
+                ]
+            ),
+            serialization=core_schema.to_string_ser_schema(),
+        )
 
     @classmethod
-    def __get_pydantic_json_schema__(cls, field_schema):
-        field_schema.update(type="string")
+    def __get_pydantic_json_schema__(
+        cls, _core_schema: core_schema.CoreSchema, handler: GetJsonSchemaHandler
+    ) -> JsonSchemaValue:
+        return handler(core_schema.str_schema())
+
+
+ManifestVersion = Annotated[Version, _VersionPydanticAnnotation]
---
+++ tests/test_semver.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from pydantic_extra_types.semver import _VersionPydanticAnnotation
+import pytest
+
+
+class SomethingWithAVersion(BaseModel):
+    version: _VersionPydanticAnnotation
+
+
+def test_valid_semver() -> None:
+    SomethingWithAVersion(version="1.2.3")
+
+
+def test_valid_semver_with_prerelease() -> None:
+    SomethingWithAVersion(version="1.2.3-alpha.1")
+
+
+def test_invalid_semver() -> None:
+    with pytest.raises(ValueError):
+        SomethingWithAVersion(version="jim.was.here")
