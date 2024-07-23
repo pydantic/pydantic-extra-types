873cbe9880981e81a9a7fd1cace0818e3750fe20
James Baker Kroner <9373098+jbkroner@users.noreply.github.com>
2024-07-22 20:01:56 -0500

    use single quotes

--- tests/test_semver.py
+++ tests/test_semver.py
@@ -8,22 +8,22 @@ class SomethingWithAVersion(BaseModel):
 
 
 def test_valid_semver() -> None:
-    SomethingWithAVersion(version="1.2.3")
+    SomethingWithAVersion(version='1.2.3')
 
 
 def test_valid_semver_with_prerelease() -> None:
-    SomethingWithAVersion(version="1.2.3-alpha.1")
+    SomethingWithAVersion(version='1.2.3-alpha.1')
 
 
 def test_invalid_semver() -> None:
     with pytest.raises(ValueError):
-        SomethingWithAVersion(version="jim.was.here")
+        SomethingWithAVersion(version='jim.was.here')
 
 
 def test_json_schema() -> None:
     assert SomethingWithAVersion.model_json_schema() == {
-        "properties": {"version": {"title": "Version", "type": "string"}},
-        "required": ["version"],
-        "title": "SomethingWithAVersion",
-        "type": "object",
+        'properties': {'version': {'title': 'Version', 'type': 'string'}},
+        'required': ['version'],
+        'title': 'SomethingWithAVersion',
+        'type': 'object',
     }
