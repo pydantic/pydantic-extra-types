ca8a8e5837598616385b71a9a876ff9e3b3e9156
07pepa <pepe@wont_share.com>
2024-09-02 23:23:01 +0200

    deprecate semver in favor of semantic_version * fold test from semver to semantic_version so it can just be deleted * extend test cases

--- tests/test_semantic_version.py
+++ tests/test_semantic_version.py
@@ -12,12 +12,14 @@ def application_object_fixture():
     return Application
 
 
-def test_valid_semantic_version(SemanticVersionObject):
-    application = SemanticVersionObject(version='1.0.0')
+@pytest.mark.parametrize('version', ['1.0.0', '1.0.0-alpha.1', '1.0.0-alpha.1+build.1', '1.2.3'])
+def test_valid_semantic_version(SemanticVersionObject, version):
+    application = SemanticVersionObject(version=version)
     assert application.version
-    assert application.model_dump() == {'version': '1.0.0'}
+    assert application.model_dump() == {'version': version}
 
 
-def test_invalid_semantic_version(SemanticVersionObject):
+@pytest.mark.parametrize('invalid_version', ['no dots string', 'with.dots.string', ''])
+def test_invalid_semantic_version(SemanticVersionObject, invalid_version):
     with pytest.raises(ValidationError):
-        SemanticVersionObject(version='Peter Maffay')
+        SemanticVersionObject(version=invalid_version)
