af48f0abde1365ad0e69c568dd2aa2121611f679
matter1-git <tudose.caralin27@gmail.com>
2024-09-06 09:06:30 +0300

    fix linting issues (final)

--- pydantic_extra_types/domain.py
+++ pydantic_extra_types/domain.py
@@ -1,5 +1,10 @@
-from pydantic import StringConstraints
 from typing import Annotated
 
-DomainStr = Annotated[str, StringConstraints(strip_whitespace=True, to_lower=True, min_length=1, max_length=253,
-                                             pattern=r'^([a-z0-9-]+(\.[a-z0-9-]+)+)$')]
+from pydantic import StringConstraints
+
+DomainStr = Annotated[
+    str,
+    StringConstraints(
+        strip_whitespace=True, to_lower=True, min_length=1, max_length=253, pattern=r'^([a-z0-9-]+(\.[a-z0-9-]+)+)$'
+    ),
+]
