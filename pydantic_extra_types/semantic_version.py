5ded2a1f1aa1d3b741cd7f133469ef63709e860a
Yasser Tahiri <yasserth19@gmail.com>
2025-01-15 17:20:47 +0100

    ✨ Add type checking support (#285)
    * ✨ Add type checking support and improve type hints across the codebase
    
    * 🔧 Remove type ignores for improved type checking in Pendulum and Semantic Version classes

--- pydantic_extra_types/semantic_version.py
+++ pydantic_extra_types/semantic_version.py
@@ -53,3 +53,7 @@ class SemanticVersion(semver.Version):
                 pattern=r'^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
             )
         )
+
+    @classmethod
+    def validate_from_str(cls, value: str) -> 'SemanticVersion':
+        return cls.parse(value)
